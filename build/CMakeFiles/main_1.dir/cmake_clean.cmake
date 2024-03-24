09f198e51b941bfad047f804117c47fc5be25999
HaoyangChen_tb14 <haoyangchen@tju.edu.cn>
2024-03-25 01:14:04 +0800

    test

---
+++ build/CMakeFiles/main_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_1.dir/src/main.cpp.obj"
+  "CMakeFiles/main_1.dir/src/main.cpp.obj.d"
+  "libmain_1.dll.a"
+  "main_1.exe"
+  "main_1.exe.manifest"
+  "main_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
